02da4547d849b9c97396e49a315f79ec9fe99bb3
Dima Mazur <rated_demon@ukr.net>
2017-03-24 16:07:32 +0200

    10 commit

---
+++ script/parallax.js
@@ -0,0 +1,5 @@
+// const athletsBlock = document.querySelectorAll('.row');
+// athletsBlock.forEach(b => console.log(b.offsetTop))
+// window.onscroll = function(event){
+// 	console.log(this.pageYOffset);
+// }
